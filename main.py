bd74615a2af07f7a6a1a3a63f0f9b2e42433159f
asya <asya.gailamazyan@yandex.ru>
2022-03-07 14:52:08 +0300

    in progress

--- main.py
+++ main.py
@@ -5,8 +5,10 @@ from config import API_link
 
 if __name__ == '__main__':
     updates = requests.get(API_link + "/getUpdates?offset=-1").json()
-    print(updates)
+    #print(updates)
 
     message = updates['result'][0]['message']
     chat_id = message['from']['id']
     text = message['text']
+
+    send_message = requests.get(API_link + f"/sendMessage?chat_id={chat_id}&text=Hello, vi iz Anglii?")
